69e4df54d1ee653e4de80d5648dc65bb599e9e8c
DanCher17 <dancher072@gmail.com>
2017-10-30 13:08:52 +0200

    rooms devices

--- rooms.cpp
+++ rooms.cpp
@@ -6,7 +6,6 @@ Rooms::Rooms(QWidget *parent) :
     ui(new Ui::Rooms)
 {
     ui->setupUi(this);
-
     // QPixmap pixamp(":/img/timeline.png");
     // ui->timeline_1->setPixmap(pixamp.scaled(ui->scrollAreaWidgetContents->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
@@ -41,3 +40,275 @@ void Rooms::on_btn_livingroom_clicked()
     this->make_trigger_active(ui->btn_livingroom);
     ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
 }
+
+void Rooms::on_pushButton_47_clicked()
+{
+    Rooms::i = 1;
+    ui->stackedWidget->setCurrentWidget(ui->page_lamp);
+}
+
+void Rooms::on_pushButton_75_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_lamp);
+}
+
+void Rooms::on_pushButton_114_clicked()
+{
+    Rooms::i = 3;
+    ui->stackedWidget->setCurrentWidget(ui->page_lamp);
+}
+
+
+void Rooms::on_pushButton_71_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_dzhakuzi);
+}
+
+void Rooms::on_pushButton_29_clicked()
+{
+    Rooms::i = 1;
+    ui->stackedWidget->setCurrentWidget(ui->page_conder);
+}
+void Rooms::on_pushButton_83_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_conder);
+}
+
+void Rooms::on_pushButton_35_clicked()
+{
+    Rooms::i = 1;
+    ui->stackedWidget->setCurrentWidget(ui->page_warmfloor);
+}
+
+void Rooms::on_pushButton_91_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_warmfloor);
+}
+
+void Rooms::on_pushButton_106_clicked()
+{
+    Rooms::i = 3;
+    ui->stackedWidget->setCurrentWidget(ui->page_warmfloor);
+}
+
+void Rooms::on_pushButton_38_clicked()
+{
+    Rooms::i = 1;
+    ui->stackedWidget->setCurrentWidget(ui->page_battery);
+}
+
+
+void Rooms::on_pushButton_87_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_battery);
+}
+
+void Rooms::on_pushButton_17_clicked()
+{
+    Rooms::i = 1;
+    ui->stackedWidget->setCurrentWidget(ui->page_rozetka);
+}
+void Rooms::on_pushButton_95_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_rozetka);
+
+}
+
+
+void Rooms::on_pushButton_99_clicked()
+{
+    Rooms::i = 2;
+    ui->stackedWidget->setCurrentWidget(ui->page_rozetka);
+}
+
+void Rooms::on_pushButton_110_clicked()
+{
+    Rooms::i = 3;
+    ui->stackedWidget->setCurrentWidget(ui->page_rozetka);
+}
+
+void Rooms::on_pushButton_79_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_coffe);
+}
+
+void Rooms::on_pushButton_41_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_humidifier);
+}
+
+void Rooms::on_pushButton_30_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+}
+
+void Rooms::on_pushButton_31_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+}
+
+void Rooms::on_pushButton_2_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_61_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+}
+
+void Rooms::on_pushButton_62_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+}
+
+void Rooms::on_pushButton_49_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_50_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_59_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_60_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_57_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_58_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_66_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+}
+
+void Rooms::on_pushButton_67_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+}
+
+void Rooms::on_pushButton_32_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+void Rooms::on_pushButton_48_clicked()
+{
+    switch(Rooms::i) {
+    case 1:
+        ui->stackedWidget->setCurrentWidget(ui->page_livingroom);
+        break;
+    case 2:
+        ui->stackedWidget->setCurrentWidget(ui->page_kitchen);
+        break;
+    case 3:
+
+        ui->stackedWidget->setCurrentWidget(ui->page_bathroom);
+        break;
+    }
+}
+
+
--- rooms.h
+++ rooms.h
@@ -13,6 +13,7 @@ class Rooms : public QWidget
 
 public:
     explicit Rooms(QWidget *parent = 0);
+    int i;
     ~Rooms();
 
 private slots:
@@ -22,6 +23,72 @@ private slots:
     void on_btn_kitchen_clicked();
 
     void on_btn_livingroom_clicked();
+
+    void on_pushButton_75_clicked();
+
+    void on_pushButton_71_clicked();
+
+    void on_pushButton_83_clicked();
+
+    void on_pushButton_29_clicked();
+
+    void on_pushButton_35_clicked();
+
+    void on_pushButton_106_clicked();
+
+    void on_pushButton_91_clicked();
+
+    void on_pushButton_87_clicked();
+
+    void on_pushButton_38_clicked();
+
+    void on_pushButton_79_clicked();
+
+    void on_pushButton_41_clicked();
+
+    void on_pushButton_30_clicked();
+
+    void on_pushButton_31_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_61_clicked();
+
+    void on_pushButton_62_clicked();
+
+    void on_pushButton_49_clicked();
+
+    void on_pushButton_50_clicked();
+
+    void on_pushButton_59_clicked();
+
+    void on_pushButton_60_clicked();
+
+    void on_pushButton_57_clicked();
+
+    void on_pushButton_58_clicked();
+
+    void on_pushButton_66_clicked();
+
+    void on_pushButton_67_clicked();
+
+    void on_pushButton_32_clicked();
+
+    void on_pushButton_48_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_110_clicked();
+
+    void on_pushButton_99_clicked();
+
+    void on_pushButton_95_clicked();
+
+    void on_pushButton_114_clicked();
+
+    void on_pushButton_47_clicked();
 
 private:
     Ui::Rooms *ui;
